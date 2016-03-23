987987b967310533ae3ecee2ff682c0e039be336
Katja Seltmann <moon@begoniasociety.org>
2016-03-22 17:15:41 -0700

    css

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
     	</td>
 	</tr>
 	<tr>
-		<td class="footer">
-<img src="https://www.wildlife.ca.gov/Portals/0/Images/marine/MPAs/plan_process_hist_info/logo_ucsb2.gif"/>The SCC portal is supported and sponsored by the Cheadle Center for Biodiversity and Ecological Restoration, University of California Santa Barbara
+		<td class="footer">		
+<img src="https://www.wildlife.ca.gov/Portals/0/Images/marine/MPAs/plan_process_hist_info/logo_ucsb2.gif"/>
+<div style="font-weight:bold;margin:-70px 0px 50px 150px;">The SCC portal is supported and sponsored by the Cheadle Center for Biodiversity and Ecological Restoration, University of California Santa Barbara.</div>
+</div>
 		</td>
 	</tr>
 </table>
