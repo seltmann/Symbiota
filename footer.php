fc060011a0267f31ea475803dc3776f51cfcdd9d
Katja Seltmann <katjaseltmann@Katjas-MacBook-Pro.local>
2018-08-09 11:55:15 -0700

    new footer

--- footer.php
+++ footer.php
@@ -2,8 +2,8 @@
 	</tr>
 	<tr>
 		<td class="footer">		
-<img src="https://www.wildlife.ca.gov/Portals/0/Images/marine/MPAs/plan_process_hist_info/logo_ucsb2.gif"/>
-<div style="font-weight:bold;margin:-70px 0px 50px 150px;">The SCC portal is supported and sponsored by the Cheadle Center for Biodiversity and Ecological Restoration, University of California Santa Barbara.</div>
+<img src=""/>
+<div style="font-weight:bold;margin:-70px 0px 50px 150px;">......</div>
 </div>
 		</td>
 	</tr>
