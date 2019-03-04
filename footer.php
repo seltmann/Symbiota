0a744b42a9306982e061bd97b02126aef2f8895c
seltmann <enicospilus@gmail.com>
2019-03-04 14:29:28 -0800

    footer

--- footer.php
+++ footer.php
@@ -2,10 +2,10 @@
 	</tr>
 	<tr>
 		<td class="footer">		
-<div><img src="https://www.wildlife.ca.gov/Portals/0/Images/marine/MPAs/plan_process_hist_info/logo_ucsb2.gif" width="100"/><img src="https://www.imls.gov/sites/default/files/imls-logo_0.png" width="150" /></div>
-<div style="font-weight:bold;margin:-75px 0px 100px 100px;">This portal is supported and sponsored by the Cheadle Center for Biodiversity and Ecological Restoration, University of California Santa Barbara.</div>
+<img src="https://www.wildlife.ca.gov/Portals/0/Images/marine/MPAs/plan_process_hist_info/logo_ucsb2.gif" width="100"/><img src="https://www.imls.gov/sites/default/files/imls-logo_0.png" width="150" />
+<div style="font-weight:bold;margin:-200px 0px 100px 100px;">This portal is supported and sponsored by the Cheadle Center for Biodiversity and Ecological Restoration, University of California Santa Barbara.</div>
 
-
+</div>
 		</td>
 	</tr>
 </table>
